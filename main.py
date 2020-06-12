cee6093e895d320f7386d1935bb8426c66c7b85a
kamran saberifard <kamisaberi@yahoo.com>
2020-06-12 23:05:59 +0430

    bugs fixed

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import os
 import time
-
+import json
 from tinydb import TinyDB, Query, where
+
+configs = json.loads(open('config/app.json').read())
+proxies = TinyDB('db/bot.json').table('proxies')
+links = TinyDB('db/bot.json').table('links')
 
 # driver_path = ChromeDriverManager().install()
 # PROXY = "49.12.75.192:3128"  # IP:PORT or HOST:PORT
 # PROXY = "178.62.113.81:3128"  # IP:PORT or HOST:PORT
 # PROXY = "98.143.145.30:62353"  # IP:PORT or HOST:PORT
 # PROXY = "169.57.1.85:8123"  # IP:PORT or HOST:PORT
-
 # PROXY = "134.209.29.120:8080"  # IP:PORT or HOST:PORT
-PROXY = "82.119.170.106:8080"  # IP:PORT or HOST:PORT
+
+
 chrome_options = webdriver.ChromeOptions()
+if 'use_proxies' in configs and configs['use_proxies']:
+    PROXY = "82.119.170.106:8080"  # IP:PORT or HOST:PORT
+    chrome_options.add_argument('--proxy-server=%s' % PROXY)
+
+# PROXY = "82.119.170.106:8080"  # IP:PORT or HOST:PORT
+# chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument('--proxy-server=%s' % PROXY)
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
@@ -26,19 +36,18 @@ driver_path = ROOT_DIR + '/drivers/browsers/chromedriver.83.exe'
 driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
 # chrome.get("https://python.org")
 # chrome.get("https://whatismyipaddress.com/")
-driver.get("http://gestyy.com/eq0dIO")
 
-timeout = 50
-
-print("start sleep")
-time.sleep(10)
-print("end sleep")
-
-# WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.TAG_NAME, "span"))).click()
-WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.ID, "skip_button"))).click()
-
-time.sleep(10)
-driver.quit()
+for l in links:
+    driver.get(l['url'])
+    timeout = 50
+    print("start sleep for" + str(l['url']))
+    time.sleep(10)
+    print("end sleep for" + str(l['url']))
+    # WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.TAG_NAME, "span"))).click()
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.ID, "skip_button"))).click()
+    time.sleep(10)
+    driver.quit()
+    time.sleep(10)
 
 # try:
 #     element_present = EC.presence_of_element_located((By.LINK_TEXT, 'SKIP THIS AD'))
